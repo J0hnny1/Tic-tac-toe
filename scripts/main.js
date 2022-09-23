b2a2d09cf9382ecf12faccb8f4070aa4e0581fa5
J0hnny1 <googoj23@gmail.com>
2022-09-23 10:48:55 +0200

    peer to peer connection for multiplayer

--- scripts/main.js
+++ scripts/main.js
@@ -39,70 +39,147 @@ document.getElementById('startGame').onclick = function () {
 //play field
 document.getElementById('t1').onclick = function () {
     if (!t1S) {
-        play('t1', 0, 0);
+        play(0, 0);
         t1S = true;
 
     }
 };
 document.getElementById('t2').onclick = function () {
     if (!t2S) {
-        play('t2', 0, 1);
+        play(0, 1);
         t2S = true;
     }
 };
 document.getElementById('t3').onclick = function () {
     if (!t3S) {
-        play('t3', 0, 2);
+        play(0, 2);
         t3S = true;
     }
 };
 document.getElementById('t4').onclick = function () {
     if (!t4S) {
-        play('t4', 1, 0);
+        play(1, 0);
         t4S = true;
     }
 };
 document.getElementById('t5').onclick = function () {
     if (!t5S) {
-        play('t5', 1, 1);
+        play(1, 1);
         t5S = true;
     }
 };
 document.getElementById('t6').onclick = function () {
     if (!t6S) {
-        play('t6', 1, 2);
+        play(1, 2);
         t6S = true;
     }
 };
 document.getElementById('t7').onclick = function () {
     if (!t7S) {
-        play('t7', 2, 0);
+        play(2, 0);
         t7S = true;
     }
 };
 document.getElementById('t8').onclick = function () {
     if (!t8S) {
-        play('t8', 2, 1);
+        play(2, 1);
         t8S = true;
     }
 };
 document.getElementById('t9').onclick = function () {
     if (!t9S) {
-        play('t9', 2, 2);
+        play(2, 2);
         t9S = true;
     }
 };
+//peerjs
+let peer
+let conn
+
+function createPeerJS() {
+    peer = new Peer();
+    peer.on('open', function (id) {
+        console.log('My peer ID is: ' + id);
+        document.getElementById("peerID").innerText = "Your ID is: " + id
+        //parent.navigator.clipboard.writeText(id);
+    });
+}
+
+function createConnection(dest) {
+    conn = peer.connect(dest);
+    console.log("connected to : " + dest)
+    conn.on("open", () => {
+        conn.send("hi!");
+    });
+
+    console.log("g");
+
+}
+
+// ISSUE der als erstes connected muss nochmal verbinden für beidseitige Connection
+document.getElementById("startMultGame").onclick = function () {
+    console.log(document.getElementById("destPlayerID").value);
+    createConnection(document.getElementById("destPlayerID").value);
+    peer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+            // Will print 'hi!'
+            console.log(data);
+            receivedState(data)
+        });
+        conn.on("open", () => {
+            conn.send("hello!");
+        });
+    });
+};
+
+//let dest;
 
 
-function play(id, x, y) {
+document.getElementById('getID').onclick = function () {
+    createPeerJS();
+};
+document.getElementById("testSend").onclick = function () {
+    conn.send("Test Message");
+};
+
+let fieldID;
+
+function play(x, y) {
+
+    if (x === 0 && y === 0) {
+        fieldID = "t1";
+    } else if (x === 0 && y === 1) {
+        fieldID = "t2";
+    } else if (x === 0 && y === 2) {
+        fieldID = "t3";
+    } else if (x === 1 && y === 0) {
+        fieldID = "t4";
+    } else if (x === 1 && y === 1) {
+        fieldID = "t5";
+    } else if (x === 1 && y === 2) {
+        fieldID = "t6";
+    } else if (x === 2 && y === 0) {
+        fieldID = "t7";
+    } else if (x === 2 && y === 1) {
+        fieldID = "t8";
+    } else if (x === 2 && y === 2) {
+        fieldID = "t9";
+    }
+    console.log("ID: " + fieldID);
     if (currentPlayer === 1) {
-        document.getElementById(id).textContent = 'X';
-        document.getElementById(id).style.color = 'blue';
-        items[x][y] = 1;
+        document.getElementById(fieldID).textContent = 'X';
+        document.getElementById(fieldID).style.color = 'blue';
+        if (items[x][y] === 0){
+            items[x][y] = 1;
+        }
+
     } else {
-        document.getElementById(id).textContent = 'O';
-        document.getElementById(id).style.color = 'red';
-        items[x][y] = 2;
+        document.getElementById(fieldID).textContent = 'O';
+        document.getElementById(fieldID).style.color = 'red';
+        if (items[x][y] === 0){
+            items[x][y] = 2;
+        }
+
     }
 
     if (currentPlayer === 2) {
@@ -118,7 +195,65 @@ function play(id, x, y) {
         checkTie();
     }
 
-    document.getElementById(id).className = "noHover";
+    document.getElementById(fieldID).className = "noHover";
+    sendState();
+}
+
+function sendState() {
+    const gameState = {items: items, currentPlayer: currentPlayer};
+
+    conn.send(gameState);
+    //conn.send(items);
+    //conn.send(currentPlayer);
+}
+
+function receivedState(data) {
+    //if (Array.isArray(data)) {
+    //    items = data;
+    //    console.log("isArray")
+    //    arrayToPlayField();
+    //}
+    items = data.items;
+    console.log("Rec items: ")
+    console.log(data.items);
+    currentPlayer = data.currentPlayer;
+
+    console.log("Items: " + items);
+    arrayToPlayField();
+}
+
+function arrayToPlayField() {
+    console.log("arrayToFIeld");
+    switch (items) {
+        case items[0][0]:
+            play(0, 0)
+            t1S = true;
+            break;
+        case items[0][1]:
+            play(0, 1);
+            break;
+        case items[0][2]:
+            play(0, 2);
+            break;
+        case items[1][0]:
+            play(1, 0);
+            break;
+        case items[2][0]:
+            play(2, 0);
+            break;
+        case items[1][1]:
+            play(1, 1);
+            break;
+        case items[2][2]:
+            play(2, 2);
+            break;
+        case items[2][1]:
+            play(2, 1);
+            break;
+        case items[1][2]:
+            play(1, 2);
+            break;
+    }
 
 }
 
