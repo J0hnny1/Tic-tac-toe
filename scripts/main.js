0ca0ded479582d5fd1f0c2711329b5f7ebad392d
J0hnny1 <googoj23@gmail.com>
2023-05-16 00:50:20 +0200

    multiplayer works

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,26 @@
 let currentPlayer = 'X';
 let multiplayer = false;
 let multiplayerPlayer
+let youArePLayer
 
 const playField = {t1: "0", t2: "0", t3: "0", t4: "0", t5: "0", t6: "0", t7: "0", t8: "0", t9: "0"};
 let fieldSet = {t1: false, t2: false, t3: false, t4: false, t5: false, t6: false, t7: false, t8: false, t9: false};
 //start game | clear screen
 document.getElementById('startGame').onclick = function () {
-    console.log('Clear pressed');
+    //reset playfield
     for (let i = 1; i < 10; i++) {
         let c = "t" + i
-        playField[c] = 0;
+        playField[c] = '0';
         fieldSet[c] = false
         document.getElementById(c).textContent = ' ';
     }
-
-
+    //reset player
     currentPlayer = 'X';
-    document.getElementById('playerOnTurn').textContent = 'Current Player: 1 (X)';
+    document.getElementById('playerOnTurn').textContent = 'Current Player: X';
     setHovers("hover")
 
     document.getElementById('playerWon').textContent = " "
+    lockField()
 };
 
 
@@ -61,65 +62,6 @@ function fieldClicked(t) {
     }
 }
 
-//peerjs
-let peer
-let conn
-
-function createPeerJS() {
-
-    multiplayer = true;
-
-    peer = new Peer();
-    peer.on('open', function (id) {
-        console.log('My peer ID is: ' + id);
-        document.getElementById("peerID").innerText = "Your ID is: " + id
-
-        //parent.navigator.clipboard.writeText(id);
-    });
-
-    if (multiplayerPlayer === 0) multiplayerPlayer = 'X'
-    else multiplayerPlayer = 'O'
-    document.getElementById("youArePlayer").innerText = "You are Player: " + multiplayerPlayer
-}
-
-function createConnection(dest) {
-    conn = peer.connect(dest);
-    console.log("connected to : " + dest)
-    conn.on("open", () => {
-        conn.send("hi!");
-    });
-
-    console.log("g");
-
-}
-
-// ISSUE der als erstes connected muss nochmal verbinden für beidseitige Connection
-document.getElementById("startMultGame").onclick = function () {
-    console.log(document.getElementById("destPlayerID").value);
-    createConnection(document.getElementById("destPlayerID").value);
-    peer.on("connection", (conn) => {
-        conn.on("data", (data) => {
-            // Will print 'hi!'
-            console.log(data);
-            receivedState(data)
-        });
-        conn.on("open", () => {
-            conn.send("hello!");
-        });
-    });
-};
-
-//let dest;
-
-
-document.getElementById('getID').onclick = function () {
-    createPeerJS();
-};
-document.getElementById("testSend").onclick = function () {
-    conn.send("Test Message");
-};
-
-//let fieldID;
 function play() {
     for (let i = 1; i < 10; i++) {
         let c = "t" + i
@@ -133,53 +75,38 @@ function play() {
             document.getElementById(c).className = "noHover";
         }
     }
-    if (currentPlayer === 'O') {
-        currentPlayer = 'X';
-    } else currentPlayer = 'O';
+    currentPlayer = (currentPlayer === 'O') ? 'X' : 'O';
+
 
     //update current player shown
-    let cpl
-    if (currentPlayer === 'X') cpl = 'Current Player: 1 (X)'
-    else cpl = 'Current Player: 2 (O)';
-    document.getElementById('playerOnTurn').textContent = cpl;
+    document.getElementById('playerOnTurn').textContent = (currentPlayer === 'X') ? 'Current Player: X' : 'Current Player: O';
 
     if (!(checkWin("X") || checkWin("O"))) {
         checkTie();
     }
 
+    if (multiplayer) lockField()
 }
 
-function sendState() {
-    const gameState = {playField: playField, player: 'O'};
+function lockField() {
+    if (currentPlayer !== youArePLayer) {
+        setHovers('noHover')
+        for (let i = 1; i < 10; i++) {
+            let c = "t" + i
+            fieldSet[c] = true
+        }
+    } else if (currentPlayer === youArePLayer) {
+        setHovers('hover')
 
-    conn.send(gameState);
-    //conn.send(items);
-    //conn.send(currentPlayer);
-}
-
-function receivedState(data) {
-    console.log("Rec items: ")
-    //console.log(data.items);
-    //currentPlayer = data.currentPlayer;
-    console.log(playField)
-
-    for (let i = 1; i < 10; i++) {
-        let c = "t" + i
-        playField[c] = data.playField[c];
-        if (data.playField[c] !== 'X' && data.playField[c] !== 'O') {
-            playField[c] = 0;
+        for (let i = 1; i < 10; i++) {
+            let c = "t" + i
+            fieldSet[c] = false
+            if (playField[c] !== '0') {
+                fieldSet[c] = true
+                document.getElementById(c).className = "noHover";
+            }
         }
     }
-    play();
-    /*
-    if (currentPlayer === 'X') currentPlayer = 'O'
-    else if (currentPlayer === 'O') currentPlayer = 'X'
-
-     */
-
-
-    //playField[data.field] = data.player
-    //play()
 }
 
 function checkWin(player) {
@@ -217,9 +144,8 @@ function checkTie() {
         playField.t7 !== '0' && playField.t8 !== '0' &&
         playField.t9 !== '0') {
 
-
         document.getElementById('playerWon').textContent = "Tie";
-        document.getElementById('playerWon').style.color = ' ';
+        document.getElementById('playerWon').style.color = '#00539F';
 
     }
 }
@@ -236,3 +162,87 @@ function setHovers(cssClass) {
     }
 }
 
+
+//peerjs
+let peer
+let conn
+let currP
+
+function createPeerJS() {
+
+    multiplayer = true;
+
+    peer = new Peer();
+    peer.on('open', function (id) {
+        console.log('My peer ID is: ' + id);
+        document.getElementById("peerID").innerText = "Your ID is: " + id
+        currP = id % 2 === 0 ? 'X' : 'O';
+        multiplayerPlayer = currP
+        if (currP === 'O') youArePLayer = 'X'
+        else if (currP === 'X') youArePLayer = 'O'
+        //console.log("pl1 _ " + youArePLayer)
+        //console.log('currP -' + currP)
+        //console.log(otherPlayer)
+        //parent.navigator.clipboard.writeText(id);
+    });
+}
+
+function createConnection(dest) {
+    conn = peer.connect(dest);
+    console.log("connected to : " + dest)
+    conn.on("open", () => {
+        const otherPlayer = youArePLayer === 'X' ? 'O' : 'X';
+        conn.send(otherPlayer)
+    });
+}
+
+// ISSUE der als erstes connected muss nochmal verbinden für beidseitige Connection
+document.getElementById("startMultGame").onclick = function () {
+    console.log(document.getElementById("destPlayerID").value);
+    createConnection(document.getElementById("destPlayerID").value);
+    peer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+            // Will print 'hi!'
+            console.log(data);
+            //console.log("rec pla" + data.p)
+
+            receivedState(data)
+        });
+        conn.on("open", () => {
+            conn.send("hello!");
+        });
+    });
+    lockField()
+};
+
+document.getElementById('getID').onclick = function () {
+    createPeerJS();
+};
+
+function sendState() {
+    const gameState = {playField: playField, player: multiplayerPlayer};
+    conn.send(gameState);
+}
+
+let pp = false
+
+function receivedState(data) {
+    if (typeof data === 'string' && data !== 'hello!' && data !== 'hi!' && !pp) {
+        console.log("Player Received is: " + data)
+        youArePLayer = data
+        console.log("pay 2 " + youArePLayer)
+        pp = true
+    }
+    document.getElementById("youArePlayer").textContent = " You are Player " + youArePLayer
+    console.log(playField)
+    for (let i = 1; i < 10; i++) {
+        let c = "t" + i
+        //if (data.playField) {
+            playField[c] = data.playField[c];
+            if (data.playField[c] !== 'X' && data.playField[c] !== 'O') {
+                playField[c] = '0';
+            }
+        //}
+    }
+    play();
+}
